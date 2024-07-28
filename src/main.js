c3ee158f5ac65f25592f9f8871b4fb036bc287fe
Singor <3395224649@qq.com>
2024-07-28 12:34:15 +0800

    新增界面

--- src/main.js
+++ src/main.js
@@ -1,6 +1,13 @@
-import './assets/main.css'
-
 import { createApp } from 'vue'
 import App from './App.vue'
+import router from "@/router/index.js";
 
-createApp(App).mount('#app')
+// 引入国际化i18n
+import i18n from '@/i18n/index.js'
+
+const app = createApp(App)
+
+app.use(router)
+app.use(i18n)
+
+app.mount('#app')
